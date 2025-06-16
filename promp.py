a620262894c4b5b4ead95e88dcb1d0c93c7a00df
Neelesh Sethi <51898600+neeleshsethi@users.noreply.github.com>
2025-06-16 22:28:08 +0530

    Create promp.py

---
+++ promp.py
@@ -0,0 +1,6 @@
+@dataclass
+class Prompt:
+    messages: List[Dict] = field(default_factory=list)
+    tools: List[Dict] = field(default_factory=list)
+    metadata: dict = field(default_factory=dict)
+  
