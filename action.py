bab4ee695a5cb17883c7a25a0a76aafa3f9a05dc
Neelesh Sethi <51898600+neeleshsethi@users.noreply.github.com>
2025-06-16 21:41:19 +0530

    Create action.py

---
+++ action.py
@@ -0,0 +1,16 @@
+class Action:
+    def __init__(self,
+                 name: str,
+                 function: Callable,
+                 description: str,
+                 parameters: Dict,
+                 terminal: bool = False):
+        self.name = name
+        self.function = function
+        self.description = description
+        self.terminal = terminal
+        self.parameters = parameters
+
+    def execute(self, **args) -> Any:
+        """Execute the action's function"""
+        return self.function(**args)
